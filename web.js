0c7016e727e32949c68f8d99799b0a5a06c48323
eggpie76 <eggpie76@naver.com>
2026-03-04 21:24:35 +0900

    3

--- web.js
+++ web.js
@@ -1,10 +1,14 @@
-export const handler = async (event) => {
+const http = require("http");
 
-  //var msg = event.queryStringParameters.key1;
-  // TODO implement
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify("abc"),
-  };
-  return response;
-};
+// HTTP 서버 생성
+const server = http.createServer((req, res) => {
+  // HTTP 응답 설정
+  res.writeHead(200, { "Content-Type": "application/json" });
+  res.end(JSON.stringify("abc"));
+});
+
+// 서버 실행 (Cafe24에서 제공하는 포트 사용)
+const PORT = process.env.PORT || 8001; // Cafe24는 PORT 환경 변수를 통해 포트를 할당합니다.
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
