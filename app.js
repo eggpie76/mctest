eac7915597a7058629a58efc3924b8d4d8a56773
eggpie76 <eggpie76@naver.com>
2026-03-06 10:35:07 +0900

    0306-1

---
+++ app.js
@@ -0,0 +1,12 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "application/json" });
+  res.end(JSON.stringify("Hello from Node.js server!"));
+});
+
+const PORT = process.env.PORT || 8001; // Cafe24에서 제공하는 포트 사용
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
