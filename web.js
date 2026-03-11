ee1248a97453685aa81ee350a4de14ae56de0276
eggpie76 <eggpie76@naver.com>
2026-03-12 07:07:15 +0900

    0312-01

--- web.js
+++ web.js
@@ -1,17 +1,15 @@
-const express = require("express");
-const app = express();
+const http = require("http");
 
-// 포트 설정
-const PORT = process.env.PORT || 8001;
-
-// GET 요청 처리
-app.get("/hello", (req, res) => {
-    const name = req.query.name || "Guest"; // 쿼리 스트링에서 이름 가져오기
-    const message = `Hello, ${name}!`;
-    res.json({ message });
+var text = "hi";
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "application/json" });
+  text += ", ok";
+  res.end(JSON.stringify(text));
 });
 
-// 서버 시작
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+const PORT = process.env.PORT || 8001; // Cafe24에서 제공하는 포트 사용
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
+
+
