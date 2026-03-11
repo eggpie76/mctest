d7a7ae86b19b80b073b6e7bb09b27d193d152ee9
eggpie76 <eggpie76@naver.com>
2026-03-12 07:14:28 +0900

    0312-03

--- web.js
+++ web.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const http = require("http");
 
-var text = "express test 01";
+var text = "express test 02: ";
+
+// 바코드로 제품 정보 검색 API
+app.get("/good/:barcode", (req, res) => {
+    const barcode = req.params.barcode;
+    text += barcode
+});
+
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "application/json" });
   text += ", ok";
