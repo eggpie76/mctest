efef0324cad366e699b9eac0221856899c9606f3
eggpie76 <eggpie76@naver.com>
2026-03-12 07:46:04 +0900

    0312-07

--- web.js
+++ web.js
@@ -1,7 +1,27 @@
 const express = require("express");
+const mysql = require("mysql2");
 
 let text = "express test 04: ";
 const app = express();
+
+// MySQL 연결 설정
+const db = mysql.createConnection({
+  host: '10.0.0.1', // Cafe24에서 제공하는 DB 호스트
+  user: 'martcalctest', // DB 사용자 이름
+  password: 'backery12!', // DB 비밀번호
+  database: 'martcalctest' // DB 이름
+});
+
+db.connect((err) => {
+    if (err) {
+	text += "Database connection failed";
+        console.error("Database connection failed:", err);
+        return;
+    }
+	text += "Connected to the database.";
+
+    console.log("Connected to the database.");
+});
 
 // 바코드로 제품 정보 검색 API
 app.get("/good/:barcode", (req, res) => {
